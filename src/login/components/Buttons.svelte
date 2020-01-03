9a75afc20efa44e8e250f5c5db597c669d7f05f9
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-01-03 06:45:54 -0600

    clean up buttons styling

--- src/login/components/Buttons.svelte
+++ src/login/components/Buttons.svelte
@@ -1,13 +1,11 @@
 <style>
-	.buttons {
-        right: 16px;
-        bottom: 10px;
-        position: fixed;
-	}
+.buttons {
+	right: 16px;
+	bottom: 10px;
+	position: fixed;
+}
 </style>
 
 <div class="buttons">
-	<slot>
-		<em>no content was provided</em>
-	</slot>
+	<slot></slot>
 </div>
