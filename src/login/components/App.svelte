ecddbedc0a337edda2a78280655f6c76278d94cb
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-01-03 06:41:50 -0600

    remove login background component
    it's not needed anymore'

--- src/login/components/App.svelte
+++ src/login/components/App.svelte
@@ -2,7 +2,6 @@
 	import Select from 'svelte-select';
 	import Box from './Box.svelte';
 	import Buttons from './Buttons.svelte';
-	import Background from './Background.svelte';
 
 	let items = [
 		{value: 'aggie', label: 'Texas A&M University'},
@@ -60,39 +59,37 @@
 	}
 </script>
 
-<Background>
-	{#if state==0}
-		<Box>
-			<h2>Welcome to Gradebook!</h2>
-			<p>There is not yet an account associated with {email}. Would you like to create one?</p>
-			<Buttons>
-				<button on:click={back}>
-					Back
-				</button>
-				<button on:click={createAccount}>
-					Create Account
-				</button>
-			</Buttons>
-		</Box>
-	{:else if state==1}
-		<Box>
-			<h2>Find Your School</h2>
-			<Select {items} isDisabled={notListed} bind:selectedValue></Select>
-			<div>
-				<input type="checkbox" bind:checked={notListed}>
-				My school isn't listed
-			</div>
-			{#if notListed}
-				My school: <input type=text bind:value={userInputName}>
-			{/if}
-			<Buttons>
-				<button on:click={back}>
-					Back
-				</button>
-				<button on:click={confirm}>
-					Confirm
-				</button>
-			</Buttons>
-		</Box>
-	{/if}
-</Background>
+{#if state==0}
+	<Box>
+		<h2>Welcome to Gradebook!</h2>
+		<p>There is not yet an account associated with {email}. Would you like to create one?</p>
+		<Buttons>
+			<button on:click={back}>
+				Back
+			</button>
+			<button on:click={createAccount}>
+				Create Account
+			</button>
+		</Buttons>
+	</Box>
+{:else if state==1}
+	<Box>
+		<h2>Find Your School</h2>
+		<Select {items} isDisabled={notListed} bind:selectedValue></Select>
+		<div>
+			<input type="checkbox" bind:checked={notListed}>
+			My school isn't listed
+		</div>
+		{#if notListed}
+			My school: <input type=text bind:value={userInputName}>
+		{/if}
+		<Buttons>
+			<button on:click={back}>
+				Back
+			</button>
+			<button on:click={confirm}>
+				Confirm
+			</button>
+		</Buttons>
+	</Box>
+{/if}
