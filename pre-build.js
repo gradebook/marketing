5b2861f2a927b0ec44117289b8c2a0027b46a673
Ramsay Smith <ramsaysmith@outlook.com>
2020-01-11 11:20:26 -0600

    use template to build privacy info

--- pre-build.js
+++ pre-build.js
@@ -1,23 +1,36 @@
 const fs = require('fs');
 const {URL} = require('url');
 const axios = require('axios');
+const marked = require('marked');
 
 const resources = [
-  ['tos.md', './terms-of-service.md'],
-  ['privacy.md', './privacy-policy.md']
+  ['tos', './terms-of-service'],
+  ['privacy', './privacy-policy'],
+  ['SECURITY', './security']
 ]
 
-function saveResource(path, destination) {
-  const stream = fs.createWriteStream(destination);
-  const url = new URL('/gradebook/legal/master/' + path, 'https://raw.githubusercontent.com/').href;
+function buildResource(path, destination) {
+  fs.unlink(destination + '.html', () => {});
+  const stream = fs.createWriteStream(destination + '.html');
 
-  return axios({method: 'GET', responseType: 'stream', url})
-    .then(response => response.data.pipe(stream));
+  return fs.readFile('template.html', (err, data) => {
+    if (err)
+      throw err;
+
+    const url = new URL('/gradebook/legal/master/' + path + '.md', 'https://raw.githubusercontent.com/').href;
+
+    return axios({method: 'GET', responseType: 'text', url})
+    .then(response => {
+      let template = data.toString();
+      template = template.replace('{{body}}', marked(response.data));
+      stream.write(template);
+    });
+  });
 }
 
-function updateResources() {
-  return Promise.all(resources.map(([remote, local]) => saveResource(remote, local)));
+function buildResources() {
+  return Promise.all(resources.map(([remote, local]) => buildResource(remote, local)));
 }
 
-updateResources()
+buildResources()
   .catch(e => console.error(e));
