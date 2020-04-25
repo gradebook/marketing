b40a7d0648db63d884d24942f6b33c60fc2ef3b5
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-04-25 17:11:48 -0500

    Make asset helper more robust

--- 11ty/helpers/asset.js
+++ 11ty/helpers/asset.js
@@ -3,14 +3,19 @@ const fs = require('fs');
 
 const MANIFEST_PATH = resolve(__dirname, '../../.cache-manifest');
 
-const manifest = JSON.parse(fs.readFileSync(MANIFEST_PATH, 'utf8') || '{}');
+let manifest;
+
+try {
+	manifest = JSON.parse(fs.readFileSync(MANIFEST_PATH, 'utf8'));
+} catch {}
 
 module.exports = file => {
-	if (process.env.ELEVENTY_ENV === 'dev') {
-		return file;
+	if (!manifest) {
+		return file.replace('scss', 'css');
 	}
 
 	const manifestKey = file.replace(/^\//, '');
+	const prefix = file.startsWith('/') ? '/' : ''
 
-	return manifestKey in manifest ? manifest[manifestKey] : manifestKey
+	return manifestKey in manifest ? prefix + manifest[manifestKey] : prefix + manifestKey.replace('scss', 'css')
 };
