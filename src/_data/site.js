72525f35fed693a92847c7af2b3cd2a5a00a41ff
Ramsay Smith <ramsaysmith@outlook.com>
2020-04-28 22:34:04 -0500

    use absolute urls for social icons
    Co-Authored-By: Vikas Potluri <vikaspotluri123.github@gmail.com>

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,5 +2,5 @@ module.exports = {
 	url: process.env.SITE_URL,
 	title: process.env.SITE_TITLE,
 	auth: process.env.AUTH_URL,
-	logo: '/static/assets/icon-social.png'
+	logo: `${process.env.SITE_URL}/static/assets/icon-social.png`
 };
