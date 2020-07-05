e0423ae4ea4e8bfa5e7f2ab010bd876e38923e8a
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-07-05 00:19:11 -0500

    convert 11ty file to tabs

--- .eleventy.js
+++ .eleventy.js
@@ -9,78 +9,78 @@ const time = require('./11ty/data-fetchers/timer');
 const helpers = require('./11ty/helpers');
 
 module.exports = function(config) {
-  // Assist RSS feed template
-  config.addPlugin(pluginRSS);
+	// Assist RSS feed template
+	config.addPlugin(pluginRSS);
 
-  // Apply performance attributes to images
-  /* config.addPlugin(lazyImages, {
-    cacheFile: ''
-  }); */
+	// Apply performance attributes to images
+	/* config.addPlugin(lazyImages, {
+		cacheFile: ''
+	}); */
 
-  // Copy images over from Ghost
-  /* config.addPlugin(localImages, {
-    distPath: 'dist',
-    assetPath: '/assets/images',
-    selector: 'img',
-    attribute: 'data-src', // Lazy images attribute
-    verbose: false
-  }); */
+	// Copy images over from Ghost
+	/* config.addPlugin(localImages, {
+		distPath: 'dist',
+		assetPath: '/assets/images',
+		selector: 'img',
+		attribute: 'data-src', // Lazy images attribute
+		verbose: false
+	}); */
 
-  config.addHandlebarsHelper('block', helpers.block);
-  config.addHandlebarsHelper('blockContent', helpers.block.content);
-  config.addHandlebarsHelper('meta', helpers.meta);
-  config.addHandlebarsHelper('offset', helpers.offset);
-  config.addHandlebarsHelper('pagination', helpers.pagination);
-  config.addHandlebarsHelper('plural', helpers.plural);
-  config.addHandlebarsHelper('absolute_url', helpers.absoluteURL);
-  config.addHandlebarsHelper('img_url', helpers.imageURL);
-  config.addFilter('asset', helpers.asset);
-  config.addFilter('reading_time', helpers.readingTime);
-  config.addFilter('date', helpers.date);
-
-
-  // Don't ignore the same files ignored in the git repo
-  config.setUseGitIgnore(false);
-
-  if (process.env.NO_FETCH !== 'true') {
-    // Get all pages, called 'docs' to prevent conflicting the eleventy page object
-    config.addCollection('docs', time(ghost.getPages, 'Fetch Pages', false));
-    config.addCollection('posts', time(ghost.getPosts, 'Fetch Posts', false));
-    config.addCollection('authors', time(ghost.getAuthors, 'Fetch Authors', false));
-    config.addCollection('tags', time(ghost.getTags, 'Fetch Tags', false));
-    config.addCollection('github', time(github, 'Fetch Legal', false));
-  }
+	config.addHandlebarsHelper('block', helpers.block);
+	config.addHandlebarsHelper('blockContent', helpers.block.content);
+	config.addHandlebarsHelper('meta', helpers.meta);
+	config.addHandlebarsHelper('offset', helpers.offset);
+	config.addHandlebarsHelper('pagination', helpers.pagination);
+	config.addHandlebarsHelper('plural', helpers.plural);
+	config.addHandlebarsHelper('absolute_url', helpers.absoluteURL);
+	config.addHandlebarsHelper('img_url', helpers.imageURL);
+	config.addFilter('asset', helpers.asset);
+	config.addFilter('reading_time', helpers.readingTime);
+	config.addFilter('date', helpers.date);
 
 
-  config.addPassthroughCopy('static');
+	// Don't ignore the same files ignored in the git repo
+	config.setUseGitIgnore(false);
 
-  /* // Display 404 page in BrowserSnyc
-  config.setBrowserSyncConfig({
-    callbacks: {
-      ready: (err, bs) => {
-        const content_404 = fs.readFileSync('dist/404.html');
+	if (process.env.NO_FETCH !== 'true') {
+		// Get all pages, called 'docs' to prevent conflicting the eleventy page object
+		config.addCollection('docs', time(ghost.getPages, 'Fetch Pages', false));
+		config.addCollection('posts', time(ghost.getPosts, 'Fetch Posts', false));
+		config.addCollection('authors', time(ghost.getAuthors, 'Fetch Authors', false));
+		config.addCollection('tags', time(ghost.getTags, 'Fetch Tags', false));
+		config.addCollection('github', time(github, 'Fetch Legal', false));
+	}
 
-        bs.addMiddleware('*', (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      }
-    }
-  }); */
 
-  // Eleventy configuration
-  return {
-    dir: {
-      input: 'src',
-      output: 'dist',
-      layouts: 'layouts'
-    },
+	config.addPassthroughCopy('static');
 
-    // Files read by Eleventy, add as needed
-    templateFormats: ['hbs', 'md'],
-    htmlTemplateEngine: 'hbs',
-    markdownTemplateEngine: 'hbs',
-    passthroughFileCopy: true
-  };
+	/* // Display 404 page in BrowserSnyc
+	config.setBrowserSyncConfig({
+		callbacks: {
+			ready: (err, bs) => {
+				const content_404 = fs.readFileSync('dist/404.html');
+
+				bs.addMiddleware('*', (req, res) => {
+					// Provides the 404 content without redirect.
+					res.write(content_404);
+					res.end();
+				});
+			}
+		}
+	}); */
+
+	// Eleventy configuration
+	return {
+		dir: {
+			input: 'src',
+			output: 'dist',
+			layouts: 'layouts'
+		},
+
+		// Files read by Eleventy, add as needed
+		templateFormats: ['hbs', 'md'],
+		htmlTemplateEngine: 'hbs',
+		markdownTemplateEngine: 'hbs',
+		passthroughFileCopy: true
+	};
 };
