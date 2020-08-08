f6c554306607b3b966710035c4aad923ce7e77a8
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-08-08 12:24:58 -0500

    pass 3 at ignored builds

--- should-build.sh
+++ should-build.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
-test $INCOMING_HOOK_URL && echo 'Webhook found' || git diff $CACHED_COMMIT_REF --name-only | egrep '(scripts|styles)'
+echo "Checking if build should run"
+
+# 0 means exit EARLY
+
+if [ -z "$INCOMING_HOOK_URL" ]; then
+	git diff $CACHED_COMMIT_REF --name-only | egrep '(scripts|styles)' -v --silent
+else
+	echo 'Webhook found'
+	exit 1
+fi
+
