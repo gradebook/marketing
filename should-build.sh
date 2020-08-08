d380f7c17f52053b7e0bf4efd9fbfe4fa0e18e26
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-08-08 13:31:10 -0500

    update should-build to also build when src folder changes

--- should-build.sh
+++ should-build.sh
@@ -5,7 +5,7 @@ echo "Checking if build should run"
 # 0 means exit EARLY
 
 if [ -z "$INCOMING_HOOK_URL" ]; then
-	git diff $CACHED_COMMIT_REF --name-only | egrep '(scripts|styles)' -v --silent
+	git diff $CACHED_COMMIT_REF --name-only | egrep '(scripts|styles|src)' -v --silent
 else
 	echo 'Webhook found'
 	exit 1
