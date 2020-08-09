4b059dddc85c432617a884f490cfb2ae9adf5c90
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-08-09 16:22:05 -0500

    Fix ignore script not recognizing real changes

--- should-build.sh
+++ should-build.sh
@@ -5,7 +5,13 @@ echo "Checking if build should run relative to $CACHED_COMMIT_REF"
 # 0 means exit EARLY
 
 if [ -z "$INCOMING_HOOK_URL" ]; then
-	git diff $CACHED_COMMIT_REF --name-only | egrep '(scripts|styles|src)' -v --silent
+	git diff $CACHED_COMMIT_REF --name-only | egrep '(scripts|styles|src)' --silent
+	CODE=$?
+	if [ "$?" -gt 0 ]; then
+		exit 0
+	else
+		exit 1
+	fi
 else
 	echo 'Webhook found'
 	exit 1
