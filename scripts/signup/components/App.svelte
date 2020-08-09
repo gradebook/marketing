720f4ad742af57b155bd3daae7cb67e5124a945f
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-08-09 16:10:04 -0500

    Refactor signup to use more modern coding standards (#49)

--- scripts/signup/components/App.svelte
+++ scripts/signup/components/App.svelte
@@ -1,153 +1,99 @@
-<style>
-.inline {
-	display: inline;
-}
-</style>
 <script>
-	import Select from 'svelte-select';
-	import Box from './Box.svelte';
-	import Buttons from './Buttons.svelte';
-	import items from './schools';
 	import {onMount} from 'svelte'
+	import Select from 'svelte-select';
+	import AsyncButton from './async-button.svelte';
+	import items from './schools';
+	import {getUser, logout, approveAccount} from '../services/net';
 	import getUrl from './get-url';
 
+	const STATE = {
+		confirmCreation: 0,
+		selectSchool: 1
+	};
+
 	let school;
-	let startingLabel = 'Select...';
 	let notListed = false;
 	let message = '';
 	let userInputName = '';
 
 	let email = 'your email';
-	let state = 0;
-
-	let focusElem;
+	let state = STATE.confirmCreation;
 	let guessedSchool;
 
-	onMount(function() {
-		focusElem.focus();
+	$: disallowSubmission = !Boolean((notListed && userInputName) || (!notListed && school));
 
-		fetch(getUrl('/api/v0/session'), {credentials: 'include'}).then(r => r.json()).then(user => {
-			email = user.email;
-			if (!user.isNew) {
-				window.location.href = getUrl('/api/v0/redirect');
-			}
-
-			const domain = email.substring(email.lastIndexOf('@') + 1);
-			guessedSchool = items.find(school => school.domain === domain);
-			school = guessedSchool;
-		}).catch(error => console.error(`__getUser::${error.message}`));
-	})
-
-	function createAccount() {
-		state = 1;
-	}
-
-	function cancel() {
-		fetch(getUrl('/api/v0/session/end'), {credentials: 'include'}).then(r => {
-			if (r.status === 204) {
-				window.location.href = getUrl('');
-			}
-		}).catch(error => console.error(`__getUser::${error.message}`));
-
-		message = 'ERROR: Please refresh the page and try again.';
-	}
-
-	function canConfirm() {
-		if(!notListed && !school) {
-			return false;
+	onMount(async () => {
+		const user = await getUser();
+		if (!user) {
+			return;
 		}
 
-		if(notListed && !userInputName) {
-			return false;
-		}
+		email = user.email;
+		const domain = email.substring(email.lastIndexOf('@') + 1);
+		guessedSchool = items.find(school => school.domain === domain);
+		school = guessedSchool;
+	});
 
-		return true;
+	async function cancel() {
+		const response = await logout();
+		if (response) {
+			message = 'ERROR: Please refresh the page and try again.';
+		}
 	}
 
-	function confirm() {
-		if(!canConfirm()) {
-			message = 'Please select or type a school.';
-			return false;
-		}
+	const setState = state_ => state = state_;
 
+	async function confirm() {
 		message = '';
-		let payload = {};
+		const payload = {};
 
-		if(!notListed & school !== undefined) {
-			payload.school = school.value;
-		} else if (notListed) {
+		if (notListed) {
 			payload.school = 'www';
 			payload.suggestion = userInputName;
+		} else {
+			payload.school = school.value;
 		}
 
-		const params = {
-			method: 'PUT',
-			body: JSON.stringify(payload),
-			headers: {
-				'content-type': 'application/json'
-			},
-			credentials: 'include'
-		};
-
-		fetch(getUrl('/api/v0/approve'), params).then(r => r.json()).then((r) => {
-			if (r.message) {
-				if (r.message === 'You are already approved') {
-					return window.location.href = getUrl('/api/v0/redirect');
-				}
-
-				message = r.message;
-				return;
-			}
-
-			if (r.domain) {
-				const domain = r.domain.replace(/\/$/, '');
-				window.location.href = `${domain}/api/v0/me/approve`;
-			} else {
-				console.log(r);
-				message = 'Unable to process response. Please contact support if this issue persists.';
-			}
-		}).catch(error => {
-			console.log(error);
-			message = error;
-		});
+		const response = await approveAccount(payload);
+		if (response) {
+			message = response;
+		}
 	}
 </script>
-
-{#if state==0}
-	<Box>
-		<h2>Welcome to Gradebook!</h2>
-		<p style="margin-top: 45px;">There is not yet an account associated with {email}. Would you like to create one?</p>
-		<Buttons>
-			<button on:click={cancel}>
-				Cancel
-			</button>
-			<button bind:this={focusElem} on:click={createAccount}>
-				Create Account
-			</button>
-		</Buttons>
-	</Box>
-{:else if state==1}
-	<Box>
-		<h2>Find Your School</h2>
-		<Select {items} selectedValue={guessedSchool} isDisabled={notListed} on:select={e => {school = e.detail; message = ''}}></Select>
-		<br>
-		<div class="space">
-			<input type="checkbox" id="not-listed" bind:checked={notListed} on:change={e => message = ''}>
-			<label for="not-listed" class="inline">My school isn't listed</label>
+<div class="box">
+	{#if state === STATE.confirmCreation}
+		<h2 class="header">Welcome to Gradebook!</h2>
+		<main class="mt-8">
+			<p>There is not yet an account associated with {email}. Would you like to create one?</p>
+		</main>
+		<div class="footer">
+			<AsyncButton click={cancel}>Cancel</AsyncButton>
+			<!-- svelte-ignore a11y-autofocus -->
+			<button autofocus on:click={() => setState(STATE.selectSchool)}>Create Account</button>
 		</div>
-		{#if notListed}
-			My school: <input class="school" type="text" bind:value={userInputName}>
-		{/if}
-		{#if message}
-			<p class="warn" style="bottom: 12px; position: absolute;">{message}</p>
-		{/if}
-		<Buttons>
-			<button on:click={cancel}>
-				Cancel
-			</button>
-			<button on:click={confirm}>
-				Confirm
-			</button>
-		</Buttons>
-	</Box>
-{/if}
+	{:else if state === STATE.selectSchool}
+		<h2 class="header">Find Your School</h2>
+		<main class="content">
+			<Select
+				{items}
+				selectedValue={guessedSchool}
+				isDisabled={notListed}
+				on:select={e => {school = e.detail; message = ''}}
+				on:clear={() => {school = null; message = ''}}
+			></Select>
+			<br>
+			<div class="space">
+				<input type="checkbox" id="not-listed" bind:checked={notListed} on:change={e => message = ''}>
+				<label for="not-listed" class="inline">My school isn't listed</label>
+			</div>
+			{#if notListed}
+				My school: <input class="school" type="text" bind:value={userInputName}>
+			{/if}
+		</main>
+		<div class="footer">
+			<p class="error-message">{message}</p>
+			<AsyncButton click={cancel}>Cancel</AsyncButton>
+			<AsyncButton disabled={disallowSubmission} click={confirm}>Confirm</AsyncButton>
+		</div>
+	{/if}
+</div>
---
+++ scripts/signup/services/net.js
@@ -0,0 +1,62 @@
+import getUrl from '../components/get-url';
+
+export function getUser() {
+	return fetch(getUrl('/api/v0/session'), {credentials: 'include'})
+		.then(response => response.json())
+		.then(user => {
+			if (!user.isNew) {
+				window.location.href = getUrl('/api/v0/redirect');
+			}
+
+			return user;
+		})
+		.catch(error => {
+			console.error(`__getUser::${error.message}`);
+			return null;
+		});
+}
+
+export function logout() {
+	return fetch(getUrl('/api/v0/session/end'), {credentials: 'include'})
+		.then(response => {
+			if (response.status === 204) {
+				window.location.href = getUrl('');
+			}
+		})
+		.catch(error => {
+			console.error(`__getUser::${error.message}`)
+			return 'ERROR: Please refresh the page and try again.';
+		});
+}
+
+export function approveAccount(payload) {
+	return fetch(getUrl('/api/v0/approve'), {
+		method: 'PUT',
+		body: JSON.stringify(payload),
+		headers: {
+			'content-type': 'application/json'
+		},
+		credentials: 'include'
+	})
+	.then(response => response.json())
+	.then(response => {
+		if (response.message) {
+			if (response.message === 'You are already approved') {
+				return window.location.href = getUrl('/api/v0/redirect');
+			}
+
+			return response.message;
+		}
+
+		if (response.domain) {
+			const domain = response.domain.replace(/\/$/, '');
+			window.location.href = `${domain}/api/v0/me/approve`;
+		} else {
+			console.log(response);
+			return 'Unable to process response. Please contact support if this issue persists.';
+		}
+	}).catch(error => {
+		console.log(error);
+		return error;
+	});
+}
