96a107c80515d3488019226b85522874311c1e36
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-08-19 22:30:28 -0500

    fix cta for the final time

--- scripts/update-cta.js
+++ scripts/update-cta.js
@@ -1,29 +1,28 @@
-{
-	const {AUTH_URL} = env;
-	fetch(`${AUTH_URL}/api/v0/session`, {credentials: 'include'}).then(r => r.json()).then(user => {
-		const bothCta = document.querySelectorAll('.js-auth-redirect');
-		let updatedText;
-		let updatedLink;
+// @ts-check
+const {AUTH_URL} = env;
+fetch(`${AUTH_URL}/api/v0/session`, {credentials: 'include'}).then(r => r.json()).then(user => {
+	const bothCta = document.querySelectorAll('.js-auth-redirect');
+	let updatedText;
+	let updatedLink;
 
-		if (user.isNew) {
-			updatedText = 'Create Your Account';
-			updatedLink = '/signup';
-		} else if (user.school) {
-			updatedText = 'Go to Dashboard';
-			updatedLink = `${AUTH_URL}/api/v0/redirect`;
-		}
+	if (user.isNew) {
+		updatedText = 'Create Your Account';
+		updatedLink = '/signup';
+	} else if (user.school) {
+		updatedText = 'Go to Dashboard';
+		updatedLink = `${AUTH_URL}/api/v0/redirect`;
+	}
 
-		if (updatedText && updatedLink) {
-			window.externalWindowMutex = {
-				requestFocus() {
-					window.location.href = updatedLink;
-				}
-			};
-
-			for (cta of bothCta) {
-				cta.setAttribute('href', updatedLink);
-				cta.textContent = updatedText;
+	if (updatedText && updatedLink) {
+		window.externalWindowMutex = {
+			requestFocus() {
+				window.location.href = updatedLink;
 			}
+		};
+
+		for (const cta of bothCta) {
+			cta.setAttribute('href', updatedLink);
+			cta.textContent = updatedText;
 		}
-	}).catch(error => console.error(`__updateCTA::${error.message}`));
-}
+	}
+}).catch(error => console.error(`__updateCTA::${error.message}`));
