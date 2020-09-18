5a6beef564a2d0a56ea1933a768b999812f56ca4
Ramsay Smith <ramsaysmith@outlook.com>
2020-09-18 14:53:31 -0500

    create stripe prod and test keys

---
+++ src/_data/stripe.js
@@ -0,0 +1,7 @@
+module.exports = process.env.NODE_ENV === 'production' ? {
+  publishable: 'pk_live_dD7GgROq0JBYy255fEC6H4vY00tNQkplqx',
+  donate: 'price_1HSaK6IHvfDCUF3iIzI8pGRc'
+} : {
+  publishable: 'pk_test_vnyB8sDZ9TN2bxFjpFC9DurQ00vZLLqtxW',
+  donate: 'price_1HSkxdIHvfDCUF3iPHN0ILsO'
+};
