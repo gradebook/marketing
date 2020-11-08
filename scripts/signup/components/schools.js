040b6fef7e07c4b1bdcb755dd29a54c5598e10ca
Josh Costa <43763502+joshcosta@users.noreply.github.com>
2020-11-08 16:18:45 -0600

    Add new schools (#72)

--- scripts/signup/components/schools.js
+++ scripts/signup/components/schools.js
@@ -1,9 +1,14 @@
 export default [
   {"value": "aggie", "label": "TAMU - Texas A&M University", "domain": "tamu.edu"},
-  {"value": "rebel", "label": "Ole Miss - University of Mississippi", "domain": "olemiss.edu"},
-  {"value": "gator", "label": "University of Florida", "domain": "ufl.edu"},
+  {"value": "blinn", "label": "Blinn College", "domain": "blinn.edu"},
+  {"value": "bobcat", "label": "TXST - Texas State University", "domain": "txstate.edu"},
+  {"value": "comet", "label": "UTD - University of Texas at Dallas", "domain": "utdallas.edu"},
   {"value": "commodore", "label": "Vanderbilt University", "domain": "vanderbilt.edu"},
-  {"value": "wolfpack", "label": "NCSU - NC State University", "domain": "ncsu.edu"},
+  {"value": "cougar", "label": "UH - University of Houston", "domain": "uh.edu"},
+  {"value": "gator", "label": "UF - University of Florida", "domain": "ufl.edu"},
+  {"value": "longhorn", "label": "UT - University of Texas at Austin", "domain": "utexas.edu"},
+  {"value": "raider", "label": "TTU - Texas Tech University", "domain": "ttu.edu"},
+  {"value": "rebel", "label": "Ole Miss - University of Mississippi", "domain": "olemiss.edu"},
   {"value": "tiger", "label": "Clemson University", "domain": "clemson.edu"},
-  {"value": "raider", "label": "Texas Tech University", "domain": "ttu.edu"}
+  {"value": "wolfpack", "label": "NCSU - NC State University", "domain": "ncsu.edu"}
 ]
