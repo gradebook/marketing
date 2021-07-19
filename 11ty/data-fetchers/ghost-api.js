fb286ac19d9c24c331fae7df59b2fa291532e94d
Vikas Potluri <vikaspotluri123.github@gmail.com>
2021-07-18 20:14:05 -0500

    extract ghost api version to a constant

--- 11ty/data-fetchers/ghost-api.js
+++ 11ty/data-fetchers/ghost-api.js
@@ -1,9 +1,11 @@
 const ghostContentAPI = require('@tryghost/content-api');
 
+module.exports.API_VERSION = 'v2';
+
 module.exports.api = new ghostContentAPI({
   url: process.env.GHOST_API_URL,
   key: process.env.GHOST_CONTENT_API_KEY,
-  version: 'v2'
+  version: module.exports.API_VERSION
 });
 
 module.exports.stripDomain = url => url.replace(process.env.GHOST_SITE_URL || process.env.GHOST_API_URL, '');
