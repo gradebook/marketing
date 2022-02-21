61bbd4642cff22fb0ceff70d80fb76eec8b1646c
Vikas Potluri <vikaspotluri123.github@gmail.com>
2022-02-21 00:18:09 -0600

    update-cta: don't run on pages without a CTA

--- scripts/update-cta.js
+++ scripts/update-cta.js
@@ -1,28 +1,31 @@
 // @ts-check
 const {AUTH_URL} = env;
-fetch(`${AUTH_URL}/api/v0/session`, {credentials: 'include'}).then(r => r.json()).then(user => {
-	const bothCta = document.querySelectorAll('.js-auth-redirect');
-	let updatedText;
-	let updatedLink;
+const CTAs = document.querySelectorAll('.js-auth-redirect');
 
-	if (user.isNew) {
-		updatedText = 'Create Your Account';
-		updatedLink = '/signup';
-	} else if (user.school) {
-		updatedText = 'Go to Dashboard';
-		updatedLink = `${AUTH_URL}/api/v0/redirect`;
-	}
+if (CTAs.length > 0) {
+	fetch(`${AUTH_URL}/api/v0/session`, {credentials: 'include'}).then(r => r.json()).then(user => {
+		let updatedText;
+		let updatedLink;
 
-	if (updatedText && updatedLink) {
-		window.externalWindowMutex = {
-			requestFocus() {
-				window.location.href = updatedLink;
-			}
-		};
-
-		for (const cta of bothCta) {
-			cta.setAttribute('href', updatedLink);
-			cta.textContent = updatedText;
+		if (user.isNew) {
+			updatedText = 'Create Your Account';
+			updatedLink = '/signup';
+		} else if (user.school) {
+			updatedText = 'Go to Dashboard';
+			updatedLink = `${AUTH_URL}/api/v0/redirect`;
 		}
-	}
-}).catch(error => console.error(`__updateCTA::${error.message}`));
+
+		if (updatedText && updatedLink) {
+			window.externalWindowMutex = {
+				requestFocus() {
+					window.location.href = updatedLink;
+				}
+			};
+
+			for (const cta of CTAs) {
+				cta.setAttribute('href', updatedLink);
+				cta.textContent = updatedText;
+			}
+		}
+	}).catch(error => console.error(`__updateCTA::${error.message}`));
+}
