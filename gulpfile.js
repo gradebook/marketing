9099c202ce40758f3144deee6d5e674ffdcd3ae3
Vikas Potluri <vikaspotluri123.github@gmail.com>
2023-02-21 11:20:08 -0800

    fix eleventy incremental builds when watching

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const {task, src, dest, parallel, series, watch} = require('gulp');
 const {GulpCssCompilerBackend} = require('./compiler-backend/gulp-css.js');
 const {JSCompilerBackend} = require('./compiler-backend/js.js');
-let eleventy;
+/** @type {import('@11ty/eleventy/src/Eleventy.js')} */
+let _eleventy;
 
 /** @type {JSCompilerBackend} */
 let jsCompilerBackend;
@@ -32,6 +33,36 @@ function createCssBackend() {
 	return gulpCssCompilerBackend.initAsync();
 }
 
+async function createEleventyBackend() {
+	if (_eleventy) {
+		await _eleventy.restart();
+	} else {
+		const Eleventy = require('@11ty/eleventy');
+		// @ts-expect-error only the config is publicly exported
+		_eleventy = new Eleventy('./src', './dist');
+	}
+
+	await _eleventy.init();
+	return _eleventy;
+}
+
+/** @param {Function | string | null} [fileOrCallback] */
+async function triggerEleventyBuild(fileOrCallback = null) {
+	const eleventy = await createEleventyBackend();
+
+	if (fileOrCallback && typeof fileOrCallback !== 'function') {
+		// @ts-expect-error we need to trigger `eleventy.resourceModified` on their event bus, and the only way to do that
+		// is to call this private function
+		_eleventy._addFileToWatchQueue(fileOrCallback);
+	}
+
+	await eleventy.write();
+
+	if (typeof fileOrCallback === 'function') {
+		fileOrCallback();
+	}
+}
+
 task('clean', () => {
 	/** @type {(glob: string) => Promise<void>} */
 	const rimraf = require('util').promisify(require('rimraf'));
@@ -52,19 +83,8 @@ task('enableWatchMode', () => {
 });
 
 task('js', createJsBackend);
-
 task('css', createCssBackend);
-
-task('html', async () => {
-	if (!eleventy) {
-		const Eleventy = require('@11ty/eleventy');
-		eleventy = new Eleventy('./src', './dist');
-		await eleventy.init();
-	}
-
-	await eleventy.write();
-	eleventy.writer.writeCount = 0;
-});
+task('html', triggerEleventyBuild);
 
 task('html:minify', () => {
 	const minify = require('./tasks/minify-html');
@@ -81,7 +101,10 @@ task('dev', series('enableWatchMode', 'default', function devServer() {
 	jsCompilerBackend.subscribe(reload);
 	gulpCssCompilerBackend.subscribe(reload)
 	watch('./styles/**/*').on('change', () => gulpCssCompilerBackend.runOnce());
-	watch(['./src/**/*.hbs','./src/**/*.md'], series('html')).on('change', reload);
+	watch(['./src/**/*.hbs','./src/**/*.md']).on('change', async file => {
+		await triggerEleventyBuild(file);
+		reload();
+	});
 }));
 
 task('build', series(
